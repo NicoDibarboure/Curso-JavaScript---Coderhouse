f9ff1ee695597caa4fdf485406ae9f4d3e924465
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-06-10 22:40:00 -0300

    01-Variables

---
+++ js/01-variables.js
@@ -0,0 +1,38 @@
+// * Alerta:
+// alert("Hola!");
+// * Mensaje en consola:
+// console.log("hola");
+
+// Declaracion de variables
+// let nombre = "Nicolas";
+// let apellido = "Dibarboure";
+// const documento = 42841167; // --> Number
+
+// * Mensaje en consola:
+
+// console.log(nombre, apellido, documento);
+
+// Declaracion de variables por promts.
+
+// let nombre = prompt("Ingrese su nombre:"); // ! TODOS LOS DATOS QUE SE INGRESAN DESDE EL NAVEGADOR LO TOMAN COMO STRING. SE DEBERIA DE PARSEAR EN TODO CASO.
+// console.log(nombre);
+
+// let nombreUsuario = prompt("Ingrese su nombre: ");
+
+// console.log("Hola " + nombreUsuario + ", ¿Cómo estas?");
+
+// TODO - Declaramos dos variables que ingresa el usuario y parseamos directamente a tipo Number.
+// let numero1 = Number(prompt("Ingrese primer numero:"));
+// let numero2 = Number(prompt("Ingrese segundo numero:"));
+// let total = numero1 + numero2;
+
+// console.log(total);
+
+// * Tipo de datos booleanos:
+//TODO: Boolean -> Booleano (True - False)
+// let estaLogeado = true;
+
+// * Tipo de datos primitivos:
+// ? String
+// ? Number
+// ? Boolean
