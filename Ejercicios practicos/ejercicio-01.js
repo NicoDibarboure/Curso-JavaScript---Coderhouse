dcae31548a1a6f3647892c8ad9b2732045d1a7bd
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-06-14 19:27:48 -0300

    Creando los ejercicios practicos y terminando el ejercicio 1.

---
+++ Ejercicios practicos/ejercicio-01.js
@@ -0,0 +1,7 @@
+let Nombre = prompt("Ingrese su nombre:");
+let Apellido = prompt("Ingrese su Apellido:");
+let Edad = Number(prompt("Ingrese su Edad:"));
+
+console.log(
+  `Su nombre completo es ${Nombre} ${Apellido} y tiene ${Edad} años.`
+);
