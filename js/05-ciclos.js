e81ca77f92391d676dd7423250fec15b26ea1f4d
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-06-19 22:39:42 -0300

    Ciclos

---
+++ js/05-ciclos.js
@@ -0,0 +1,9 @@
+// * Ciclo FOR.
+
+// ? for(inicializacion; condicion; modificador)
+
+let numero = Number(prompt("Ingrese un numero:"));
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} * ${i} = ${numero * i}`);
+}
