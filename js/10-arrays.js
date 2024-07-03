686b03119b1fce06fb058be3bf2a392e3bd46d65
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-07-03 20:45:45 -0300

    Prop Length en arrays

--- js/10-arrays.js
+++ js/10-arrays.js
@@ -8,3 +8,16 @@ let numero3 = 52;
 
 let numeros = [2, 32, 52, 12, 23];
 let palabras = ["Hola", "Mundo", "Adios"];
+
+// Los arrays trabajan por posiciones (Indice). --> Empiezan en 0.
+
+console.log(numeros[0]);
+
+let nombre = "Juan";
+console.log(nombre[0]); // J
+
+// Propiedad --> length
+
+console.log(palabras.length);
+
+// Siempre la ultima posicion de un array es el length - 1.
