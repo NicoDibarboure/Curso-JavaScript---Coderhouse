263f518bd5bc1ba746df5c260b1b9536dea5ed72
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-07-03 20:36:45 -0300

    Arrays init

---
+++ js/10-arrays.js
@@ -0,0 +1,10 @@
+// Array --> Coleccion de datos
+
+let numero1 = 2;
+let numero2 = 32;
+let numero3 = 52;
+
+// Los arrays tienen elementos --- Los objetos tienen propiedades y metodos.
+
+let numeros = [2, 32, 52, 12, 23];
+let palabras = ["Hola", "Mundo", "Adios"];
