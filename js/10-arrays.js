36e61da77ecdfd01251cedcf63bf08efbb494a2d
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-07-04 12:02:07 -0300

    Arrays y metodos

--- js/10-arrays.js
+++ js/10-arrays.js
@@ -1,49 +1,103 @@
-// Array --> Coleccion de datos
+// array ---> coleccion
 
-let numero1 = 2;
-let numero2 = 32;
-let numero3 = 52;
+// let numero1 = 2
+// let numero2 = 5
+// let numero3 = 12
 
-// Los arrays tienen elementos --- Los objetos tienen propiedades y metodos.
+// los arrays tienen elementos ---- los objetos tiene prop y metodos
+let numerosDeLaSuerte = [2, 5, 12];
+let palabras = ["casa", "avion", "pepe", "juan"];
 
-let numeros = [2, 32, 52, 12, 23];
-let palabras = ["Hola", "Mundo", "Adios"];
+// trabajan por posiciones ---> empiezan en 0
+console.log(palabras[0]);
 
-// Los arrays trabajan por posiciones (Indice). --> Empiezan en 0.
+let nombre1 = "jorge";
 
-console.log(numeros[0]);
+console.log(nombre1[0]);
 
-let nombre = "Juan";
-console.log(nombre[0]); // J
-
-// Propiedad --> length
+// prop ---> length
 
 console.log(palabras.length);
+// siempre la ultima posicion de un array es la longitud - 1
 
-// Siempre la ultima posicion de un array es el length - 1.
+console.log(palabras[0]);
+console.log(palabras[palabras.length - 1]);
 
-for (let i = 0; i < palabras.length; i++) {
-  palabras[i] += "!";
+console.log(nombre1.length);
+
+let numeros = [5, 2, 7, 4, 9];
+
+// numeros[1] = 15;
+
+// console.log(numeros[0]);
+// console.log(numeros[1]);
+// console.log(numeros[2]);
+// console.log(numeros[3]);
+// console.log(numeros[4]);
+console.log("Aca empieza el for");
+for (let i = 0; i < numeros.length; i++) {
+  //   numeros[i] = numeros[i] + 10;
+  numeros[i] += 10;
 }
 
-console.log(palabras);
+console.log(numeros);
 
-// Arrays de objetos
+// arrays de objetos
 
-let telefonos = [
+// let telefono = {
+//     nombre: "dsada",
+//     precio: 123
+// }
+// let telefono2 = {
+//     nombre: "sadas",
+//     precio: 321321
+// }
+// let telefono3 = {
+//     nombre: "dsadasdas",
+//     precio: 5412431321
+// }
+
+// for (let i = 0; i < telefonos.length; i++) {
+//   console.log(telefonos[i].nombre);
+//   console.log("--------");
+// }
+
+// let n  = 2
+
+// n = n + 5
+// n += 5
+
+let numeritos = [1, 6, 2, 7, 1];
+console.log(numeritos.length); // la longitud
+
+console.log(numeritos);
+let x = numeritos.push("pepe", "juan", 14, true, {}, "algo mas");
+console.log(x);
+
+let existe = numeritos.includes("juan"); // boolean
+console.log(existe);
+
+numeritos.pop();
+numeritos.pop();
+let z = numeritos.pop(); // devuelve el elemento eliminado
+
+console.log(z);
+
+const telefonos = [
   {
-    marca: "iPhone",
-    modelo: "12 Pro Max",
-    color: "Gris",
-    precio: 1200,
-    stock: 15,
+    nombre: "motorola",
+    precio: 30,
+    stock: 3,
   },
   {
-    marca: "Samsung",
-    modelo: "Galaxy S21",
-    color: "Negro",
-    precio: 1200,
-    stock: 15,
+    nombre: "samsung",
+    precio: 20,
+    stock: 1,
+  },
+  {
+    nombre: "iphone",
+    precio: 100,
+    stock: 5,
   },
 ];
 
@@ -53,12 +107,35 @@ class Telefono {
   }
 }
 
-let calcularVentaTotal = () => {
-  // Devolver el total que ganaria
+// let seguimos = confirm("Queres agregar otro telefono?");
+// console.log(seguimos);
+// while( seguimos ){
 
-  let valorTotal = 0;
+// }
+
+const agregarTelefonos = () => {
+  let nombre = prompt("ingrese el nombre");
+  let precio = Number(prompt("ingrese el precio"));
+  let stock = Number(prompt("ingrese el stock"));
+
+  let telefonoNuevo = new Telefono(nombre, precio, stock);
+
+  telefonos.push(telefonoNuevo);
+};
+
+let continuar = confirm("quieres agregar un nuevo telefono?");
+while (continuar) {
+  agregarTelefonos();
+  continuar = confirm("quieres agregar otro?");
+}
+
+const calcularVentaTotal = () => {
+  // devolver el total que ganaria
+
+  let valorTotal = 0; // 90 // 110 // 610
 
   for (let i = 0; i < telefonos.length; i++) {
+    // valorTotal = valorTotal + telefonos[i].precio * telefonos[i].stock;
     valorTotal += telefonos[i].precio * telefonos[i].stock;
   }
 
@@ -68,21 +145,22 @@ let calcularVentaTotal = () => {
 let totalVendido = calcularVentaTotal();
 console.log(totalVendido);
 
-let numeritos = [1, 5, 7, 8, 9];
-console.log(numeritos.length);
+console.log(telefonos);
 
-console.log(numeritos);
-let x = numeritos.push("pepe", "juan", 14, true, {}, "algo mas"); // Push sirve para agregar al final de un array
-console.log(x);
+// function pepe (){}
+// let pepe = ()=>{}
+// let pepe = function(){}
 
-let existe = numeritos.includes("juan"); // Siempre el metodo INCLUDES devuelve un booleano.
-console.log(existe);
+let frase = "hola como estas";
+let palabritas = ["pepe", "casa", "abeja", "y no se "];
 
-// Pop sirve para eliminar el ultimo elemento de un array.
+console.log(frase.length);
+let fraseMayuscula = frase.toUpperCase();
+console.log(frase);
+console.log(fraseMayuscula);
 
-numeritos.pop();
-numeritos.pop();
+console.log(frase.indexOf("z")); // -1
+console.log(palabritas.indexOf("casa")); // -1
 
-let z = numeritos.pop();
-
-console.log(z);
+// console.log(frase.includes("a"));
+// console.log(palabritas.includes("casa"));
